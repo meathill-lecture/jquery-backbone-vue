d20e07c5b8a44307a063e0fc25b2892c675e1271
Meathill <meathill@gmail.com>
2017-03-16 19:03:50 +0800

    vue 也写完了。还是 vue 快。
    Signed-off-by: Meathill <meathill@gmail.com>

---
+++ sample/vue/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by realm on 2017/3/16.
+ */
+
+const KEY = 'todos-vue';
+let store = localStorage.getItem(KEY);
+store = store ? JSON.parse(store) : [];
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    todos: store
+  },
+  methods: {
+    store() {
+      let json = JSON.stringify(this.todos);
+      localStorage.setItem(KEY, json);
+    },
+    onChange(index, event) {
+      this.todos[index].status = event.target.checked;
+      this.store();
+    },
+    onSubmit(event) {
+      let todo = event.target.elements.todo.value;
+      if (todo) {
+        this.todos.push({
+          status: false,
+          label: todo
+        });
+        event.target.elements.todo.value = '';
+        this.store();
+      }
+    }
+  }
+});
